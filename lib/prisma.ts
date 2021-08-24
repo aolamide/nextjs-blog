9b8e567067951a31621f381c84a832e07f346570
Olamide <aboyejiolamide15@gmail.com>
2021-08-24 16:58:40 +0100

    reverse prisma code

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,21 +1,15 @@
-import {PrismaClient} from "@prisma/client";
+// lib/prisma.ts
+import { PrismaClient } from '@prisma/client';
 
-const prismaClientPropertyName = `__prevent-name-collision__prisma`;
-type GlobalThisWithPrismaClient = typeof globalThis & {
-	[prismaClientPropertyName]: PrismaClient;
-};
+let prisma: PrismaClient;
 
-const getPrismaClient = () => {
-	if (process.env.NODE_ENV === `production`) {
-		return new PrismaClient();
-	} else {
-		const newGlobalThis = globalThis as GlobalThisWithPrismaClient;
-		if (!newGlobalThis[prismaClientPropertyName]) {
-			newGlobalThis[prismaClientPropertyName] = new PrismaClient();
-		}
-		return newGlobalThis[prismaClientPropertyName];
-	}
-};
-const prisma= getPrismaClient();
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
 
 export default prisma;
